6a60cc5240fab729fd2396d8edaf2235783ef9e6
HugoBast33 <96108832+HugoBast33@users.noreply.github.com>
2021-12-14 11:27:52 +0100

    Update acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -1,11 +1,24 @@
-
-
 <html>
 
 <h1> MY GIGA PROJECT TAH BILL GATES </h1>
 
+
+<p> this site look so cool </p>
+
+<form action="acceuil.php" method="post">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="text" name="age" /></p>
+ <p><input type="submit" value="OK"></p>
+</form>
+
+
 </html>
- 
+
+
 <?php
-  
-  ?>
+
+echo $_POST['nom'].'\n'.$_POST['age'];
+
+
+
+?>
